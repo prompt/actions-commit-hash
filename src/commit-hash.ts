b7f55906d059a3163d3527790e6beb08da6afcdf
Samuel Ryan <sam@samryan.co.uk>
2021-06-08 20:31:31 +0100

    feat: Provide `short` (7 char) commit hash

---
+++ src/commit-hash.ts
@@ -0,0 +1,23 @@
+const hashFormat: RegExp = /^[a-f0-9]{40}$/
+
+export class CommitHash {
+  private hash: string
+
+  constructor(longCommitHash: string) {
+    if (!hashFormat.test(longCommitHash)) {
+      throw Error(
+        `Input "${longCommitHash}" is not a valid full length commit hash.`
+      )
+    }
+
+    this.hash = longCommitHash
+  }
+
+  long(): string {
+    return this.hash
+  }
+
+  short(): string {
+    return this.long().substr(0, 7)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core'
+import {CommitHash} from './commit-hash'
 
 async function run(): Promise<void> {
   try {
-    const hash: string = core.getInput('commit')
+    const commit: CommitHash = new CommitHash(core.getInput('commit'))
 
-    core.setOutput('long', hash)
+    core.setOutput('long', commit.long())
+    core.setOutput('short', commit.short())
   } catch (error) {
     core.setFailed(error.message)
   }
